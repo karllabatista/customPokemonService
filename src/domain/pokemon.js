5196b0e72746193dcc4cad786a0f7b72a495fa58
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-21 01:11:00 -0400

    feat(domain): implement bussiness rule: - power level up of Pokemon between 1 and 100

--- src/domain/pokemon.js
+++ src/domain/pokemon.js
@@ -24,5 +24,15 @@ class Pokemon{
 
         
     }
-}
 
+    powerLevelUp(){
+
+        if (this.powerLevel >=100){
+            throw new Error("Pokemon power level must be between 1 and 100");
+        } 
+
+        this.powerLevel +=1;
+ 
+    }
+
+}
