23a74d212d365780637198a98d138e9b72d3e965
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-21 22:52:30 -0400

    test: add PokemonApiHttpClient tests

---
+++ src/infrastructure/ports/api/pokemonApiHttpClient.test.js
@@ -0,0 +1,44 @@
+import {PokemonApiHttpClient} from "./pokemonApiHttpClient.js";
+import { PokemonError } from "../../../domain/errors/PokemonError.js";
+import { expect, jest } from '@jest/globals';
+describe("PokemonApiHttpClient",()=> {
+
+    test("should call API with correct url", async () => {
+        const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+        const fetchFnMock = jest.fn().mockResolvedValue(
+            {
+                ok: true,
+                json: jest.fn().mockResolvedValue({ name: "pikachu" }),
+            }
+        );
+        const pokemon = "pikachu";
+
+        const client = new PokemonApiHttpClient(baseUrl, fetchFnMock);
+
+        await client.fetchPokemonByName(pokemon);
+
+        expect(fetchFnMock).toHaveBeenCalledWith(`${baseUrl}/${pokemon}`);
+    });
+
+    test("should retuns data response with success",async()=>{
+
+        const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+        mockData ={"name":"pikachu","height":4}
+        const fetchFnMock= jest.fn().mockReturnValue(
+            {
+                ok: true,
+                json: jest.fn().mockReturnValue(mockData)
+            }
+        );
+        const pokemon = "pikachu";
+
+        const client = new PokemonApiHttpClient(baseUrl,fetchFnMock);
+
+        result = await client.fetchPokemonByName(pokemon);
+
+        expect(fetchFnMock).toHaveBeenCalledWith(`${baseUrl}/pikachu`);
+        
+        expect(result).toEqual(mockData);
+    });
+
+});
