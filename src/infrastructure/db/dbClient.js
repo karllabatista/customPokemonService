56c6aa0f562ef1912e83fa387b1e4d898754dd5c
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-22 13:17:50 -0400

    feat(infra): add MongoDBClient to  manages Mong Connection

---
+++ src/infrastructure/db/dbClient.js
@@ -0,0 +1,65 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+class MongoDBClient{
+    /**
+     * Manages the communicate between client and MongoDB 
+     *
+     */
+
+    constructor(mongo_uri){
+        /**
+         * @param {} mongo_uri - It is the connection string address of pokemon collection
+         *                      loadded by env variables
+         *  @throws {Error} - When mongo URI is not setted
+         */
+
+        if (!mongo_uri){
+            throw new Error("MongoDB URI is required to start MongoDBClient");
+
+        }
+
+        this.client = new MongoClient(mongo_uri);
+        
+    }
+
+    async connect(){
+
+        if (!this.client.isConnected()){
+            await this.client.connect();
+            console.info("Connected to mongo");
+        }else{
+            console.info("Already connect to Mongo");
+        }
+        
+        return this.client
+    
+    }
+
+    getCollection(){
+        const pokemonCollection = client.db().collection("pokemons");
+
+        return pokemonCollection
+    }
+
+    async disconnect(){
+        // TODO MANAGE OPEN/CLOSE CONNECT
+        await this.client.close();
+    }
+
+
+};
+
+export {MongoClient}
+async function main(){
+
+    dotenv.config();
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+    await mongoClient.connect();
+    console.log("✅ Conexão com MongoDB estabelecida!");
+    await mongoClient.close();
+
+}
+
+main();
