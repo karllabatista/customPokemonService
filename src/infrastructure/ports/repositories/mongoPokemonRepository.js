fb89e3f3058c541e71f1ec79d939ba71d680e68c
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-22 00:54:55 -0400

    feat(infra) : add implemenation of Mongo Client as Pokemon Repository

---
+++ src/infrastructure/ports/repositories/mongoPokemonRepository.js
@@ -0,0 +1,44 @@
+import { PokemonRepositoryError } from "../../../domain/errors/PokemonRepositoryError";
+import { Collection } from "mongodb";
+
+class MongoPokemonRepository extends PokemonRepository{
+    /**
+     * Implementation of MongoDB Client as Pokemon Repository
+     * @param {Collection} collection - Pokemons collection
+     */
+
+    constructor(collection){
+        super();
+        this.collection = collection;
+        
+    }
+    
+    async save(customizedPokemon){
+        /**
+         * Save a Pokemon with attributes
+         * @param {Pokemon} - A pokemon object 
+         * @throws {PokemonRepositoryError} When DB operation fails
+         * 
+         * */
+
+        try{
+
+            const pokemonDoc = {
+                pokemonName: customizedPokemon.pokemonName, 
+                nickname: customizedPokemon.nickname,
+                favorite: customizedPokemon.favorite,
+                powerLevel: customizedPokemon.powerLevel
+            };
+
+            await this.collection.insertOne(pokemonDoc);
+            return pokemonDoc;
+            
+        }catch(err){
+            console.error(`[MongoPokemonRepository] Error to save Pokemon: ${err.message}`);
+            throw new PokemonRepositoryError(`Error to save Pokemon`);
+        }
+    }
+
+}
+
+export {MongoPokemonRepository}
