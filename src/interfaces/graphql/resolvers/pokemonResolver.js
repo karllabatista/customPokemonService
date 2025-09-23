2a66c57632598995ab75fd6b0f247e34720b2646
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-22 23:29:59 -0400

    refactor(interface): Improve PokemonResolver -  add PokemonFactory to handle raw data from graphql api - add handle error to  createPokemonUc.execute

--- src/interfaces/graphql/resolvers/pokemonResolver.js
+++ src/interfaces/graphql/resolvers/pokemonResolver.js
@@ -1,13 +1,21 @@
 import { CreatePokemonWithExtrasAttributesUseCase } from "../../../uses_cases/createPokemonWithExtrasAttributes.js";
-
+import { PokemonFactory } from "../../../infrastructure/factory/pokemonFactory.js";
 export const pokemonResolver ={
 
     Mutation:{
 
-        createPokemonAttributes: async (_,args,{createPokemonUc}) =>{
-
-            return await createPokemonUc.execute(args)
-
+        createPokemonAttributes: async (_, { input }, { createPokemonUc }) => {
+            try {
+                const inputDTO = PokemonFactory.fromInput(input);
+                console.log("[Resolver] DTO:", inputDTO);
+                return await createPokemonUc.execute(inputDTO);
+            } catch (err) {
+                console.error("[Resolver] Error creating Pokemon:", err);
+                throw new Error(err.message); // Apollo vai mostrar o erro
+            }
         }
-    }
-};
+    }   
+}
+
+
+
