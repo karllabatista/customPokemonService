a29a399356eae4e511c7c5f0d50713903b33a405
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 14:47:38 -0400

    chore: update script

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ import { CreatePokemonWithExtrasAttributesUseCase } from "../src/uses_cases/crea
 import { PokemonApiHttpClient } from "../src/infrastructure/ports/api/pokemonApiHttpClient.js";
 import { MongoPokemonRepository } from "../src/infrastructure/ports/repositories/mongoPokemonRepository.js";
 import { MongoDBClient } from "../src/infrastructure/db/dbClient.js";
+import { ListPokemonsUseCase } from "../src/uses_cases/listPokemons.js";
+
 import dotenv from "dotenv";
 
 // SETTING CONFIGS
@@ -12,7 +14,59 @@ dotenv.config();
 const baseUrl ="https://pokeapi.co/api/v2/pokemon";
 
 
-const run = async ()=>{
+// const run = async ()=>{
+//     //CONNECT TO MONGO SERVER
+//     const mongoClient = new MongoDBClient(process.env.MONGO_URI);
+//     await mongoClient.connect();
+
+//     //CONNECT AND USER DB
+//     const db = mongoClient.useDb();
+
+//     let input={
+//             pokemonName: "ditto",
+//             nickname: "pika-pik",
+//             favorite: true,
+//             powerLevel: 2
+//     }
+
+//     const repoPokemon = new MongoPokemonRepository(db);
+//     const apiPokemon = new PokemonApiHttpClient(baseUrl);
+
+//     const createPokemonUc = new CreatePokemonWithExtrasAttributesUseCase(apiPokemon,repoPokemon)
+    
+//     try{
+//         const result = await createPokemonUc.execute(input)
+//         console.log(result);
+
+//     }catch (error) {
+//         console.error('Erro to create Pokemon:', error);
+//     }
+    
+   
+
+//     //DISCONECT TO BD
+//     await mongoClient.disconnect()
+
+// };
+
+const pokemons = async () =>{
+
+    const apiPokemon = new PokemonApiHttpClient(baseUrl);
+    let pokemonsList =[];
+    try {
+        const limit = 20;
+        const page = 4;
+        const data = await apiPokemon.paginatePokemons(limit,page);
+        // console.log(data);
+    } catch (error) {
+
+        
+    }
+    
+};
+
+const paginatePokemons = async ()=>{
+
     //CONNECT TO MONGO SERVER
     const mongoClient = new MongoDBClient(process.env.MONGO_URI);
     await mongoClient.connect();
@@ -20,35 +74,22 @@ const run = async ()=>{
     //CONNECT AND USER DB
     const db = mongoClient.useDb();
 
-    let input={
-            pokemonName: "ditto",
-            nickname: "pika-pik",
-            favorite: true,
-            powerLevel: 2
-    }
 
-    const repoPokemon = new MongoPokemonRepository(db);
     const apiPokemon = new PokemonApiHttpClient(baseUrl);
+    const repoPokemon = new MongoPokemonRepository(db);
 
-    const createPokemonUc = new CreatePokemonWithExtrasAttributesUseCase(apiPokemon,repoPokemon)
-    
-    try{
-        const result = await createPokemonUc.execute(input)
-        console.log(result);
+    const listPokemonsUc = new ListPokemonsUseCase(apiPokemon,repoPokemon);
 
-    }catch (error) {
-        console.error('Erro to create Pokemon:', error);
-    }
-    
-   
+    let result =  await listPokemonsUc.listPokemons(10,5);
+    console.log(result);
 
     //DISCONECT TO BD
     await mongoClient.disconnect()
 
-};
+}
 
-run();
+//run();
+//pokemons();
 
-
-
+paginatePokemons();
 
