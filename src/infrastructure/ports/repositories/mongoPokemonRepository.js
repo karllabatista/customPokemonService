2894593fe5104ff8458873a2293ee05007bfe801
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 00:56:25 -0400

    feat(infra) : replace  insertOne with updateOne to store data - Use updateOne + findOne to check and store an existent  pokemon or store a new pokemon

--- src/infrastructure/ports/repositories/mongoPokemonRepository.js
+++ src/infrastructure/ports/repositories/mongoPokemonRepository.js
@@ -1,6 +1,7 @@
 import { PokemonRepository } from "../../../domain/ports/repositories/PokemonRepository.js";
 import { PokemonRepositoryError } from "../../../domain/errors/PokemonRepositoryError.js";
 import { Collection } from "mongodb";
+import { PokemonError } from "../../../domain/errors/PokemonError.js";
 
 class MongoPokemonRepository extends PokemonRepository{
     /**
@@ -32,10 +33,33 @@ class MongoPokemonRepository extends PokemonRepository{
                 powerLevel: customizedPokemon.powerLevel
             };
 
-            await this.collection.insertOne(pokemonDoc); // if collection doesn not exists, 
-                                                        //create to first insert
-            
-            return pokemonDoc;
+           // await this.collection.insertOne(pokemonDoc);
+
+            // busca o pokemon existente primeiro
+            const existing = await this.collection.findOne({ pokemonName: pokemonDoc.pokemonName });
+            const newPowerLevel = existing ? existing.powerLevel + pokemonDoc.powerLevel : pokemonDoc.powerLevel;
+
+            if (newPowerLevel < 1 || newPowerLevel > 100) {
+                throw new PokemonError("The power Level must be between 1 and 100 after update");
+            }
+
+            await this.collection.updateOne(
+                { pokemonName: pokemonDoc.pokemonName },
+                {
+                    $set: {
+                        nickname: pokemonDoc.nickname,
+                        favorite: pokemonDoc.favorite,
+                        powerLevel: newPowerLevel
+                    }
+                },
+                { upsert: true }
+            );
+
+            // busca o documento atualizado
+            const savedPokemon = await this.collection.findOne({ pokemonName: pokemonDoc.pokemonName });
+
+            return savedPokemon;
+
             
         }catch(err){
             console.error(`[MongoPokemonRepository] Error to save Pokemon: ${err.message}`);
