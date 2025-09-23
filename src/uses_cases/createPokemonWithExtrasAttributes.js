6658156e6ef8fc8f321da7370e45b89b83677b1c
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 18:15:07 -0400

    refactor(CreatePokemonWithExtrasAttributesUseCase):Remove unnecessary comments and spaces

--- src/uses_cases/createPokemonWithExtrasAttributes.js
+++ src/uses_cases/createPokemonWithExtrasAttributes.js
@@ -34,12 +34,6 @@ class CreatePokemonWithExtrasAttributesUseCase{
             // find data about pokemon in PokeAPI
             const pokemonDataApi = await this.apiPokemon.fetchPokemonByName(inputDTO.pokemonName);
 
-            //bussiness rule btw 1 and 100 - first  checked
-            // if (inputDTO.powerLevel <1 || inputDTO.powerLevel > 100){
-
-            //     throw new PokemonError(" The power Level must be between 1 and 100")
-
-            // }
 
         
             //create a customizedPokemon
@@ -55,12 +49,6 @@ class CreatePokemonWithExtrasAttributesUseCase{
             //check pokemon power level
             const existing = await this.repositoryPokemon.findByName(customizedPokemon.pokemonName);
 
-            // se retorna algo entao pokemon exists
-            ///pega power level do banco e soma com novo power
-            ///aplica regra de negocio
-            ////se passar na regra 
-            ////manda pra funcao de save
-            ////nessa funcao resove como salva
 
             let newPowerLevel = null;
             if (existing){
@@ -75,26 +63,9 @@ class CreatePokemonWithExtrasAttributesUseCase{
                 customizedPokemon.powerLevel =  newPowerLevel;
             }
 
-            // Só podem existir no máximo 3 Pokémons marcados como favoritos no
-            // MongoDB.
-            // Se o usuário tentar adicionar um 4º favorito, a mutation deve retornar erro.
-
-            //passou da regra 1
-            // vamos para regra 2
-            // do favorite
-            // vai no banco e conta total de favorite com true
-            // se o total de favorite > 3
-            //nao manda o pokemon c para o save
 
             let totalPokemonsFavorites = await this.repositoryPokemon.countFavorites();
-            //console.log("favorite type:", typeof customizedPokemon.favorite, customizedPokemon.favorite);
-
-            // //business rule  favorite >3
-            // if (totalPokemonsFavorites >= 3){
-
-            //     throw new PokemonError("Already exists three pokemons marked as favorite. Impossible save");
-            // }
-
+  
             if (existing){
                 // exists in bd
                 if( !existing.favorite && customizedPokemon.favorite==true && totalPokemonsFavorites>=3){
@@ -111,12 +82,6 @@ class CreatePokemonWithExtrasAttributesUseCase{
                 }
             }
 
-
-
-
-
-
-                    
             //save this pokemon
             const pokemonDB = await this.repositoryPokemon.save(customizedPokemon);
             
