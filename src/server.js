724bbd68a3f7eb20acb72ec4fcee452a139f6743
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-22 22:24:43 -0400

    build: add Apolo Server to expose Api by GraphQL

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./interfaces/graphql/schemas/index.js";
+import { pokemonResolver } from "./interfaces/graphql/resolvers/pokemonResolver.js";
+import { context } from "./interfaces/graphql/context.js";
+
+
+const server = new ApolloServer({ typeDefs, pokemonResolver });
+
+const { url } = await startStandaloneServer(server, {
+    context,
+    listen: { port: 4000 } });
+
+console.log(`Servidor rodando em: ${url}`);
