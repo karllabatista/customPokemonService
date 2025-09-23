8bfced6242e45f1b012e2c9e5c7b8616cc4f68a3
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 18:55:52 -0400

    feat(domain): add  validatorFavoritePokemons function

--- src/domain/validators/PokemonValidator.js
+++ src/domain/validators/PokemonValidator.js
@@ -8,5 +8,20 @@ function validatorPowerLevel(newPowerLevel){
 
 
 }
+function validatorFavoritePokemons(extistedPokemon,customizedPokemon,totalPokemonsFavorites){
+  if (extistedPokemon){
+      // exists in bd
+      if( !extistedPokemon.favorite && customizedPokemon.favorite==true && totalPokemonsFavorites>=3){
 
-export {validatorPowerLevel};
+              throw new PokemonError("Already exists three pokemons marked as favorite. Impossible save");
+          }
+
+      }else{
+          //new pokemom with favorite equals true but  totalPokemonsFavorites>=3
+          if (customizedPokemon.favorite && totalPokemonsFavorites>=3 ){
+              throw new PokemonError("Already exists three pokemons marked as favorite. Impossible save");
+          }
+      }
+
+  }
+export {validatorPowerLevel,validatorFavoritePokemons};
