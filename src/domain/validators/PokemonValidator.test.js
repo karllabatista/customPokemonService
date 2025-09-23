95b43437fe049d4234f40eee04c40c53879acf89
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 19:27:01 -0400

    testes(domain): Add tests to bussiness rules -The powerLevel field must be between 1 and 100. Otherwise, a GraphQL error will be thrown. -Só podem existir no máximo 3 Pokémons marcados como favoritos

---
+++ src/domain/validators/PokemonValidator.test.js
@@ -0,0 +1,64 @@
+import { validatorPowerLevel, validatorFavoritePokemons } from "../validators/PokemonValidator.js";
+import { PokemonError } from "../errors/PokemonError.js";
+
+describe("validatorPowerLevel", () => {
+  test("should not throw error when power level is within range", () => {
+    expect(() => validatorPowerLevel(50)).not.toThrow();
+    expect(() => validatorPowerLevel(1)).not.toThrow();
+    expect(() => validatorPowerLevel(100)).not.toThrow();
+  });
+
+  test("should throw error when power level is less than 1", () => {
+    expect(() => validatorPowerLevel(0)).toThrow(PokemonError);
+    expect(() => validatorPowerLevel(-5)).toThrow(" The power Level must be between 1 and 100");
+  });
+
+  test("should throw error when power level is greater than 100", () => {
+    expect(() => validatorPowerLevel(101)).toThrow(PokemonError);
+    expect(() => validatorPowerLevel(999)).toThrow(" The power Level must be between 1 and 100");
+  });
+});
+
+describe("validatorFavoritePokemons", () => {
+  test("should not throw error when adding a favorite Pokémon and there are less than 3 favorites", () => {
+    const existing = null;
+    const customized = { favorite: true };
+    const totalFavorites = 2;
+
+    expect(() => validatorFavoritePokemons(existing, customized, totalFavorites)).not.toThrow();
+  });
+
+  test("should throw error when trying to add a new favorite Pokémon when there are already 3 favorites", () => {
+    const existing = null;
+    const customized = { favorite: true };
+    const totalFavorites = 3;
+
+    expect(() => validatorFavoritePokemons(existing, customized, totalFavorites))
+      .toThrow("Already exists three pokemons marked as favorite. Impossible save");
+  });
+
+  test("should not throw an error when saving an existing Pokémon that remains unfavorite", () => {
+    const existing = { favorite: false };
+    const customized = { favorite: false };
+    const totalFavorites = 3;
+
+    expect(() => validatorFavoritePokemons(existing, customized, totalFavorites)).not.toThrow();
+  });
+
+  test("should throw error when trying to update an existing Pokémon to favorite when there are already 3 favorites", () => {
+    const existing = { favorite: false };
+    const customized = { favorite: true };
+    const totalFavorites = 3;
+
+    expect(() => validatorFavoritePokemons(existing, customized, totalFavorites))
+      .toThrow("Already exists three pokemons marked as favorite. Impossible save");
+  });
+
+  test("should not throw error when updating existing Pokémon that was already favorited", () => {
+    const existing = { favorite: true };
+    const customized = { favorite: true };
+    const totalFavorites = 3;
+
+    expect(() => validatorFavoritePokemons(existing, customized, totalFavorites)).not.toThrow();
+  });
+});
