37c512423758d0c88358369c365ca6dae1f057a4
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-23 22:07:11 -0400

    refactor(domain): Remove methods does not used

--- src/domain/entities/Pokemon.js
+++ src/domain/entities/Pokemon.js
@@ -15,21 +15,6 @@ class Pokemon{
         this.powerLevel = powerLevel; 
     }
 
-
-    powerLevelUp(){
-
-        if (this.powerLevel >=100){
-            throw new Error("Pokemon power level must be between 1 and 100");
-        } 
-
-        this.powerLevel +=1;
- 
-    }
-
-    markAsFavorite(){
-        this.favorite = true;
-    }
-
 }
 
 export {Pokemon}
